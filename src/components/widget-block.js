53130f96eb1458158fd237b62d8a47b52be42240
RiaKara <ria.karakasis1@students.mq.edu.au>
2024-05-06 16:16:43 +1000

    deleting the rock paper scissors game from the widget block as well

--- src/components/widget-block.js
+++ src/components/widget-block.js
@@ -29,35 +29,9 @@ class WidgetBlock extends LitElement {
   render() {
     return html`
         <h3>${this.header}</h3>
-        <h3>Rock, Paper, Scissors Game</h3>
-        <button @click="${() => this.startplaying('rock')}">Rock</button>
-        <button @click="${() => this.startplaying('paper')}">Paper</button>
-        <button @click="${() => this.startplaying('scissors')}">Scissors</button>
-        <p id="outcome"></p>
       `;
     }
   
-    startplaying(playerSelection) {
-      const selections = ['rock', 'paper', 'scissors'];
-      const systemSelection = selections[Math.floor(Math.random() * selections.length)];
-      const resultFeature = this.shadowRoot.getElementById('outcome');
-      const outcome = this.WinORLoss(playerSelection, systemSelection);
-      resultFeature.textContent = `You chose ${playerSelection}. Computer chose ${systemSelection}. ${outcome}`;
-    }
-  
-    WinORLoss(playerSelection, systemSelection) {
-      if (playerSelection === systemSelection) {
-        return "tie!";
-      } else if (
-        (playerSelection === 'rock' && systemSelection === 'scissors') ||
-        (playerSelection === 'paper' && systemSelection === 'rock') ||
-        (playerSelection === 'scissors' && systemSelection === 'paper')
-      ) {
-        return 'win!';
-      } else {
-        return 'System wins!';
-      }
-    }
   }
   
 
