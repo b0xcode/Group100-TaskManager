e4da010f999eeafc3c6437569b10ad83ef4d5b03
RiaKara <ria.karakasis1@students.mq.edu.au>
2024-05-10 01:02:50 +1000

    Entering my css code which is includes designing - The grid, - Colour and font of the GamerB and GamerA display, - The colour and aesthetic of the restart button, - The colour of the board's background - The title font size - The margin of the board display - The height and width of the grid columns and rows - etc.

--- src/components/game-widget.js
+++ src/components/game-widget.js
@@ -15,4 +15,136 @@ class GameWidget extends LitElement {
     this.GameFrame = ['', '', '', '', '', '', '', '', ''];
     this.GameStatus = true;
   }
+
+
+
+static styles = css`
+    :host {
+        display: block;
+        width: 400px;
+        height: 550px;
+        background-color: #ADDFFF;
+        color: #1F456E;
+        text-align: center;
+
+
+
+
+    }
+
+
+
+    :host input {
+        width: 5em;
+    }
+
+
+
+
+    .header {
+      font-size: 23px;
+    }
+   
+
+
+
+
+    .display {
+        color: white;
+        font-size: 25px;
+        text-align: center;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+
+
+
+    .hide {
+        display: none;
+    }
+
+
+
+
+    .BLOCK-CONTAINER {
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        grid-template-rows: repeat(3,1fr);
+        width: 80%;
+        height: 10%;
+    }
+
+
+
+
+    .grid {
+        border: 1px solid white;
+        min-width: 100px;
+        min-height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 50px;
+        cursor: pointer;
+    }
+
+
+
+
+    .GamerA {
+        color: #F94449;
+        font-weight:bold;
+        font-size: 20px;
+    }
+
+
+
+
+    .GamerB{
+        color: #498AFB;
+    }
+
+
+
+
+    .settings {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-top: 0.5em;
+    }
+
+
+
+
+    .settings button {
+        color: white;
+        padding: 10px;
+        border-radius: 15px;
+        font-size: 24px;
+        border:none;
+        cursor: pointer;
+    }
+
+
+
+
+    .restart {
+        background-color: #F94449;
+    }
+
+
+
+
+    #restart {
+        background-color: #F94449;
+    }
+`;
+
+
+
+
 }
