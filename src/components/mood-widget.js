52f4d682ce8ed4948b183ff93955eadfe7f1ee55
FrancineJeddMorales <124853205+FrancineJeddMorales@users.noreply.github.com>
2024-05-11 18:48:36 +1000

    finished functionality and design of mood-widget

--- src/components/mood-widget.js
+++ src/components/mood-widget.js
@@ -1,60 +1,77 @@
 import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
-
 class MoodWidget extends LitElement {
-    static properties = {
-
-    }
-
+  
     static styles = css `
     :host {
         display: block;
         width: 250px;
         height: 250px;
-        background-color: beige;
+        background-color: lightblue;
         text-align: center;
         
     }
     h2 {
         text-align: center;
+        color: black;
+        font-family: Papyrus;
     }
 
     .Happy {
       background: yellow;
       color: darkgreen;
+      font-family: Papyrus;
 
     }
 
     .Sad{
       background: blue;
       color: white;
+      font-family: Papyrus;
     }
 
     .Anger{
       background: red;
+      font-family: Papyrus;
+    }
+    .Hope{
+      background: lightgreen;
+      font-family: Papyrus;
     }
     `
+    happyButton(){
+      this.style.background = 'yellow';
+    }
+
+    sadButton(){
+      this.style.background = 'midnightblue';
+    }
+
+    angerButton(){
+      this.style.background = 'crimson';
+    }
+
+    hopeButton(){
+      this.style.background = 'lightgreen';
+    }
+
     connectedCallback() {
         super.connectedCallback();
+        this.addeventListener('click',this.happyButton);
+        this.addeventListener('click',this.sadButton);
+        this.addeventListener('click',this.angerButton);
+        this.addeventListener('click',this.hopeButton);
       }
-
 
     render (){
         return html`
-        <h2>Mood Setter</h2>
-        <button class="Happy">Joy</button>
-        <button class="Sad">Sadness</button>
-        <button class="Anger">Anger</button>
-
-        
-        
+        <h2>Mood Colour Setter</h2>
+        <button class="Happy" @click=${this.happyButton}>Joy</button>
+        <button class="Sad" @click=${this.sadButton} >Sadness</button>
+        <button class="Anger" @click=${this.angerButton}>Anger</button>
+        <button class="Hope" @click=${this.hopeButton}>Hopeful</button>
         `;
     }
-
-
-
-
-
 }
 
 customElements.define('mood-widget', MoodWidget);
